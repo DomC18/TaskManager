56df8aec2b63eabebeb000d35efd265f74aa764d
DomC18 <24DCatania@student.nchstx.org>
2024-04-23 11:49:49 -0500

    moved login functions into loginutil file

---
+++ loginutil.py
@@ -0,0 +1,101 @@
+import tkinter as tk
+import constants
+import globalvar
+import json
+import taskutil
+
+def verify_existing(root:tk.Tk, first_entry:tk.Entry, user_entry:tk.Entry, password_entry:tk.Entry, first_label:tk.Label, user_label:tk.Label, password_label:tk.Label, task_func) -> None:
+    data:dict = {}
+    name = first_entry.get()
+
+    name_invalid = first_entry.get() == ""
+    username_invalid = user_entry.get() == ""
+    password_invalid = password_entry.get() == ""
+
+    if (name_invalid or username_invalid or password_invalid):
+        if name_invalid:
+            first_label.configure(fg="red")
+        else: 
+            first_label.configure(fg="black")
+
+        if username_invalid:
+            user_label.configure(fg="red")
+        else: 
+            user_label.configure(fg="black")
+
+        if password_invalid:
+            password_label.configure(fg="red")
+        else: 
+            password_label.configure(fg="black")
+    
+    file_dir = constants.USERDATADIR + name + ".json"
+    try:
+        with open(file_dir, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        return
+        
+    username = data[name]["username"]
+    password = data[name]["password"]
+
+    globalvar.name = name
+    globalvar.username = username
+    globalvar.password = password
+
+    if username == user_entry.get() and password == password_entry.get():
+        root.destroy()
+        taskutil.load_tasks()
+        task_func()
+
+def register_new(root:tk.Tk, first_entry:tk.Entry, user_entry:tk.Entry, password_entry:tk.Entry, first_label:tk.Label, user_label:tk.Label, password_label:tk.Label, task_func) -> None:
+    data:dict = {}
+    name = first_entry.get()
+    username = user_entry.get()
+    password = password_entry.get()
+
+    name_invalid = name == ""
+    username_invalid = username == ""
+    password_invalid = password == ""
+
+    if (name_invalid or username_invalid or password_invalid):
+        if name_invalid:
+            first_label.configure(fg="red")
+        else: 
+            first_label.configure(fg="black")
+
+        if username_invalid:
+            user_label.configure(fg="red")
+        else: 
+            user_label.configure(fg="black")
+
+        if password_invalid:
+            password_label.configure(fg="red")
+        else: 
+            password_label.configure(fg="black")
+
+        return
+    
+    file_dir = constants.USERDATADIR + name + ".json"
+
+    try:
+        open(file_dir, "r")
+        return True
+    except FileNotFoundError:
+        pass
+
+    data = {
+        name: {
+            "username": username,
+            "password": password
+        }
+    }
+
+    globalvar.name = name
+    globalvar.username = username
+    globalvar.password = password
+
+    with open(file_dir, "w") as file:
+        json.dump(data, file, indent=4)
+
+    root.destroy()
+    task_func()
--- ui.py
+++ ui.py
@@ -1,107 +1,12 @@
 from listbox import Listbox
+from loginutil import register_new, verify_existing
 import tkinter as tk
 import globalvar
 import constants
 import taskutil
-import json
 
-def verify_existing(root:tk.Tk, first_entry:tk.Entry, user_entry:tk.Entry, password_entry:tk.Entry, first_label:tk.Label, user_label:tk.Label, password_label:tk.Label) -> None:
-    data:dict = {}
-    name = first_entry.get()
-
-    name_invalid = first_entry.get() == ""
-    username_invalid = user_entry.get() == ""
-    password_invalid = password_entry.get() == ""
-
-    if (name_invalid or username_invalid or password_invalid):
-        if name_invalid:
-            first_label.configure(fg="red")
-        else: 
-            first_label.configure(fg="black")
-
-        if username_invalid:
-            user_label.configure(fg="red")
-        else: 
-            user_label.configure(fg="black")
-
-        if password_invalid:
-            password_label.configure(fg="red")
-        else: 
-            password_label.configure(fg="black")
-    
-    file_dir = constants.USERDATADIR + name + ".json"
-    try:
-        with open(file_dir, "r") as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        return
-        
-    username = data[name]["username"]
-    password = data[name]["password"]
-
-    globalvar.name = name
-    globalvar.username = username
-    globalvar.password = password
-
-    if username == user_entry.get() and password == password_entry.get():
-        root.destroy()
-        taskutil.load_tasks()
-        init_task_interface()
-
-def register_new(root:tk.Tk, first_entry:tk.Entry, user_entry:tk.Entry, password_entry:tk.Entry, first_label:tk.Label, user_label:tk.Label, password_label:tk.Label) -> None:
-    data:dict = {}
-    name = first_entry.get()
-    username = user_entry.get()
-    password = password_entry.get()
-
-    name_invalid = name == ""
-    username_invalid = username == ""
-    password_invalid = password == ""
-
-    if (name_invalid or username_invalid or password_invalid):
-        if name_invalid:
-            first_label.configure(fg="red")
-        else: 
-            first_label.configure(fg="black")
-
-        if username_invalid:
-            user_label.configure(fg="red")
-        else: 
-            user_label.configure(fg="black")
-
-        if password_invalid:
-            password_label.configure(fg="red")
-        else: 
-            password_label.configure(fg="black")
-
-        return
-    
-    file_dir = constants.USERDATADIR + name + ".json"
-
-    try:
-        open(file_dir, "r")
-        return True
-    except FileNotFoundError:
-        pass
-
-    data = {
-        name: {
-            "username": username,
-            "password": password
-        }
-    }
-
-    globalvar.name = name
-    globalvar.username = username
-    globalvar.password = password
-
-    with open(file_dir, "w") as file:
-        json.dump(data, file, indent=4)
-
-    root.destroy()
-    init_task_interface()
-
-
+task_list : Listbox
+root : tk.Tk
 
 def init() -> None:
     global root
@@ -135,16 +40,14 @@ def init() -> None:
     password_entry = tk.Entry(root, show="*", font=("Arial", 25), justify="left")
     password_entry.grid(row=3, column=1, padx=10, pady=5)
 
-    login_button = tk.Button(root, text="Login", font=("Arial", 25), bg="#4CAF50", fg="white", width=15, justify="center", command=lambda r=root, f=firstname_entry, u=username_entry, p=password_entry, fl=firstname_label, ul=username_label, pl=password_label : verify_existing(r,f,u,p,fl,ul,pl))
+    login_button = tk.Button(root, text="Login", font=("Arial", 25), bg="#4CAF50", fg="white", width=15, justify="center", command=lambda r=root, f=firstname_entry, u=username_entry, p=password_entry, fl=firstname_label, ul=username_label, pl=password_label, func=init_task_interface : verify_existing(r,f,u,p,fl,ul,pl,func))
     login_button.grid(row=4, column=0, columnspan=2, pady=20)
 
-    register_label = tk.Button(root, text="Don't have an account? \nRegister after inputting credentials.", font=("Arial", 20), bg="#f0f0f0", justify="center", command=lambda r=root, f=firstname_entry, u=username_entry, p=password_entry, fl=firstname_label, ul=username_label, pl=password_label : register_new(r,f,u,p,fl,ul,pl))
+    register_label = tk.Button(root, text="Don't have an account? \nRegister after inputting credentials.", font=("Arial", 20), bg="#f0f0f0", justify="center", command=lambda r=root, f=firstname_entry, u=username_entry, p=password_entry, fl=firstname_label, ul=username_label, pl=password_label, func=init_task_interface : register_new(r,f,u,p,fl,ul,pl,func))
     register_label.grid(row=5, column=0, columnspan=2, pady=5)
 
     root.mainloop()
 
-task_list : Listbox
-root : tk.Tk
 
 def init_task_interface() -> None:
     global task_list
